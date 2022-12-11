bdd7bd7783b4dce8008c5f3786817ab4bd44d15b
azzygota <88272570+azzygota@users.noreply.github.com>
2022-12-11 17:24:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6281515860089', '6281334177340', '6283128734012'] // Put your number here
+global.owner = ['6289513081052', '6283820726489', '6285860928363'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -27,8 +27,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'Nurutomo'
+  var sticker_name = 'STICKER \'ADRIAN'
+  var sticker_author = '𝕬𝕯_𝕻𝕽𝕰𝕾𝖊𝕿  ꩜'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
